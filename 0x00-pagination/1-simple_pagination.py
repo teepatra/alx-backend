07a3764d5742d615a67e2056ae18ffb0e2910ae6
“teepatra” <fagboretomilola@gmail.com>
2024-06-25 11:41:35 +0000

    fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,14 +1,16 @@
-i#!/usr/bin/env python3
-
+#!/usr/bin/env python3
+"""Simple pagination sample.
 """
-This module contains the simple pagination class
-And the helper functions to practice cursor based
-API pagination
-"""
-
 import csv
-import math
 from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -17,51 +19,29 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """
-        The constructor
+        """Initializes a new Server instance.
         """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
-            with open(self.DATA_FILE, "r", encoding="utf-8") as f:
+            with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
         return self.__dataset
 
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """
-        The Actuall helper function
-        Args:
-            page (int):
-            page_size (int):
-        Returns:
-            Returns an tupple of ints
-        """
-        if isinstance(page, int) and isinstance(page_size, int):
-            return ((page - 1) * page_size, page_size * page)
-        raise TypeError('Expected `page` and `page_size` to be ints')
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
         """
-        Get paginated data
-        Args:
-            page (int)
-            page_size (int)
-        """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
         data = self.dataset()
-
-        try:
-            (start, end) = index_range(page, page_size)
-            return data[start:end]
-        except IndexError:
+        if start > len(data):
             return []
+        return data[start:end]
+
